0e92fccca32a6e277196f9a011f08371a63c6ab1
Florian Mueller <florian.mueller-4@mni.thm.de>
2025-07-02 14:59:33 +0200

    smth

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -663,10 +663,19 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {
+          settings = {
+            ['rust-analyzer'] = {
+              check = {
+                command = 'clippy',
+                -- extra_args = { --[[ ... ]] },
+              },
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -965,18 +974,19 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
+
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
@@ -1003,6 +1013,42 @@ require('lazy').setup({
     },
   },
 })
+
+-- Deutscher Spellchecker mit <leader>lg
+vim.api.nvim_set_keymap('n', '<leader>lg', [[:setlocal spell! spelllang=de<CR>]], { noremap = true, silent = true })
+
+-- Englischer Spellchecker mit <leader>le
+vim.api.nvim_set_keymap('n', '<leader>le', [[:setlocal spell! spelllang=en<CR>]], { noremap = true, silent = true })
+
+-- Spellchecker ausschalten mit <leader>ll
+vim.api.nvim_set_keymap('n', '<leader>ll', [[:setlocal nospell<CR>]], { noremap = true, silent = true })
+
+vim.g.vimtex_view_method = 'zathura'
+
+vim.g.vimtex_quickfix = 0
+
+vim.g.vimtex_compiler_latexmk = {
+  options = {
+    '-lualatex',
+    '-verbose',
+    '-file-line-error',
+    '-synctex=1',
+    '-interaction=nonstopmode',
+    '-shell-escape',
+  },
+}
+
+vim.wo.relativenumber = true
+
+require('toggleterm').setup {
+  open_mapping = [[<C-t>]], -- Passe den Shortcut hier an
+  direction = 'float',
+}
+
+require('platformio').setup {
+  lsp = 'clangd', --default: ccls, other option: clangd
+  -- If you pick clangd, it also creates compile_commands.json
+}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,68 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'lervag/vimtex',
+    lazy = false, -- lazy-loading will disable inverse search
+    config = function()
+      vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_compiler_latexmk = {
+        options = {
+          '-outdir=out',
+        },
+      }
+    end,
+  },
+  'aspeddro/gitui.nvim',
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {--[[ things you want to change go here]]
+    },
+  },
+  'tpope/vim-surround',
+  'anurag3301/nvim-platformio.lua',
+  dependencies = {
+    { 'akinsho/nvim-toggleterm.lua' },
+    { 'nvim-telescope/telescope.nvim' },
+    { 'nvim-lua/plenary.nvim' },
+  },
+  {
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
+  },
+}
